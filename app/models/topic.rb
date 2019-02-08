902f803fa8aef8b8c5ab07b83011d0134e79f9fb
森亮太 <moriryouta@moriryoutanoMacBook-ea.local>
2019-02-08 20:22:23 +0900

    update23

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < ApplicationRecord
   validates :user_id, presence: true
-  validates :description, presence:true, length:{maximum: 300}
+  validates :description, presence:true, length:{maximum: 255}
   validates :title, presence: true, length:{maximum: 40}
 
   belongs_to :user
